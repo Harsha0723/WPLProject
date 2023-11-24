28dd5607abad6b2b97a21bbacbb2862811d25793
Harsha0723 <113648073+Harsha0723@users.noreply.github.com>
2023-11-24 17:56:41 -0600

    Signup and Signin Pages UI

---
+++ furniture-react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import SignInSide from './pages/SignInSide';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <SignInSide/>
+      {/* <SignUp/> */}
+    </div>
+  );
+}
+
+export default App;
