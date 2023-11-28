326507e42791c060b38741353a693567610a2d2e
Krittika Paul <krittika@Krittikas-MBP.lan>
2023-11-27 21:56:02 -0600

    Initial shop changes

---
+++ furniture-react-app/src/pages/Shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Shop(props) {
+    
+
+    return (
+        <>
+            <div>
+                Welcome Shop!
+            </div>
+        </>
+    )
+}
+
+export default Shop;
--- furniture-react-app/src/routers/Routers.js
+++ furniture-react-app/src/routers/Routers.js
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
-import Home from "../pages/Home"
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
 
 const Routers = () => {
     return <Routes>
         <Route path="/" element={<Navigate to = 'home' />} />
         <Route path="home" element= {<Home/>} />
+        <Route path="shop" element= {<Shop/>} />
         <Route path="signup" element= {<SignUp/>} />
         <Route path="signin" element= {<SignIn/>} />
     </Routes>
