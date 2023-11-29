adc77504e8ae06ea7274b37e1e0d725ace641bd4
VarshaGoli2 <varshagoli2@gmail.com>
2023-11-29 02:51:33 -0600

    second commit

--- furniture-react-app/src/pages/Home.jsx
+++ furniture-react-app/src/pages/Home.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import {Link} from "react-router-dom";
 import {motion} from "framer-motion";
+import products from '../assets/data/products';
+
 import Helmet from "../components/Helmet/Helmet";
 import "../styles/home.css";
 import { Container, Row, Col} from "reactstrap";
 import heroImg from '../assets/images/hero-img.png';
 
+import Services from "../services/Services";
+import ProductsList from "../components/UI/ProductsList";
+
+import counterImg from '../assets/images/counter-timer-img.png';
+
 const Home = () => {
 
+    const [trendingProducts, settrendingProducts] = useState([]);
+    const [bestSalesProducts, setBestSalesProducts] = useState([]);
     const year=new Date().getFullYear()
+
+    useEffect(() =>{
+        const filteredTrendingProducts =products.filter(item => item.category ==='chair'
+        );
+        const filteredBestSalesProducts =products.filter(item => item.category ==='sofa'
+        );
+       settrendingProducts(filteredTrendingProducts);
+       setBestSalesProducts(filteredBestSalesProducts);
+    }, []);
     return <Helmet title ={"Home"}>
     <section className="hero__section">
         <Container>
@@ -34,6 +52,41 @@ const Home = () => {
         </Row>
         </Container>
     </section>
+
+    <Services />
+    <section className="trending__products">
+        <Container>
+            <Row>
+                <Col lg='12' className="text-center">
+                    <h2 className="section__title">Trending Products</h2>
+                </Col>
+                <ProductsList data ={trendingProducts}/>
+            </Row>
+        </Container>
+    </section>
+    <section className="best__sales">
+        <Container>
+        <Row>
+                <Col lg='12' className="text-center">
+                    <h2 className="section__title">Best Sales</h2>
+                </Col>
+                <ProductsList data ={bestSalesProducts} />
+            </Row>
+        </Container>
+    </section>
+    
+    <section className="timer__count">
+        <Container>
+            <Row>
+                <Col lg ='6' md='6'></Col>
+                <Col lg='6' md='6' className="text-end">
+                    <img src={counterImg} alt="" />
+                </Col>
+            </Row>
+        </Container>
+    </section>
+
+
     </Helmet>;
 }
 
