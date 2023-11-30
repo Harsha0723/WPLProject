12675cfff598448fe6fe2455d4c17b5b0a576d68
VarshaGoli2 <varshagoli2@gmail.com>
2023-11-30 02:06:59 -0600

    responsive

--- furniture-react-app/src/components/Footer/Footer.jsx
+++ furniture-react-app/src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <footer className="footer">
             <Container>
                 <Row>
-                    <Col lg="4">
+                    <Col lg="4" className="mb-4">
                     <div className="logo">
                         <div>
                             <h1 className="text-white">FurniMart</h1>
@@ -24,7 +24,7 @@ const Footer = () => {
                     
                     </Col>
 
-                    <Col lg="3">
+                    <Col lg="3" className="mb-4" md='6'>
                     <div className="footer__quick-links">
                         <h4 className="quick__links-title">Top Categories</h4>
                         <ListGroup className="mb-3">
@@ -38,7 +38,7 @@ const Footer = () => {
                     </div>
                     </Col>
 
-                    <Col lg="2">
+                    <Col lg="2" md='3' className="mb-4">
                     <div className="footer__quick-links">
                         <h4 className="quick__links-title">Useful Links</h4>
                         <ListGroup className="mb-3">
@@ -58,7 +58,7 @@ const Footer = () => {
                     </div>
                     </Col>
 
-                    <Col lg="3">
+                    <Col lg="3" md='4'>
                     <div className="footer__quick-links">
                         <h4 className="quick__links-title">Contact</h4>
                         <ListGroup className="footer__contact">
