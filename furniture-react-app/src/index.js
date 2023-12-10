45310fc332dc5041619cb8baf949e241e5d8f8a4
N-D-P <anshp9918@gmail.com>
2023-12-10 14:27:46 -0600

    Fixed duplicate API calls

--- furniture-react-app/src/index.js
+++ furniture-react-app/src/index.js
@@ -14,7 +14,6 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
     <Provider store = {store}>
       <ToastContainer
@@ -27,7 +26,6 @@ root.render(
         <App />
     </Provider>
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
